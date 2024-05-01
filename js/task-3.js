ea40f09c95c61332212718accec57c1c9d794102
Vlas <111683706+Kijoin1@users.noreply.github.com>
2024-05-01 13:10:00 +0300

    1.2

--- js/task-3.js
+++ js/task-3.js
@@ -3,21 +3,21 @@ const profile = {
   playTime: 300,
 
   changeUsername(newName) {
-    profile.username = newName;
+    this.username = newName;
   },
   updatePlayTime(hours) {
-    profile.playTime += hours;
+    this.playTime += hours;
   },
   getInfo(){
-    return `${profile.username} has ${profile.playTime} active hours!`
+    return `${this.username} has ${this.playTime} active hours!`
   }
 };
 
 
-// console.log(profile.getInfo());
+console.log(profile.getInfo());
 
-// profile.changeUsername("Marco");
-// console.log(profile.getInfo());
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
 
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo());
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
